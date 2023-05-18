f06f8283ae2c8ee3c6cd1a9c26c0f7a89922afb5
Jacek Pasierb <jack_byk@o2.pl>
2023-05-18 11:11:01 +0200

    Stworzono klase widget

--- src/components/Widget/Widget.jsx
+++ src/components/Widget/Widget.jsx
@@ -1,35 +1,84 @@
-import React from 'react';
+import { Component } from 'react';
 import css from './WidgetStyle.module.css';
 
-const Widget = () => {
+class Widget extends Component {
+  // constructor(){
+  // super()
+  //   }
 
-
-
-  return (
-    <div className={css.widget}>
-      <h1>Please Leave Feedback</h1>
-      <div className={css.btnsWidget}>
-        <button
-          type="submit"
-          className={`${css.btnWidget}  ${css.btnWidgetText}`}
-        >
-          Good
-        </button>
-        <button
-          type="submit"
-          className={`${css.btnWidget}  ${css.btnWidgetText}`}
-        >
-          Neutral
-        </button>
-        <button
-          type="submit"
-          className={`${css.btnWidget}  ${css.btnWidgetText}`}
-        >
-          Bad
-        </button>
+  render() {
+    return (
+      <div className={css.widget}>
+        <h1>Please Leave Feedback</h1>
+        <div className={css.btnsWidget}>
+          <button
+            type="submit"
+            className={`${css.btnWidget}  ${css.btnWidgetText}`}
+          >
+            Good
+          </button>
+          <button
+            type="submit"
+            className={`${css.btnWidget}  ${css.btnWidgetText}`}
+          >
+            Neutral
+          </button>
+          <button
+            type="submit"
+            className={`${css.btnWidget}  ${css.btnWidgetText}`}
+          >
+            Bad
+          </button>
+        </div>
+        <h3>Statistics</h3>
+        <ul>
+          <li>
+            <p>Good:</p>
+          </li>
+          <li>
+            <p>Neutral:</p>
+          </li>
+          <li>
+            <p>Bad:</p>
+          </li>
+        </ul>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Widget;
+
+// import React from 'react';
+// import css from './WidgetStyle.module.css';
+
+// const Widget = () => {
+
+//   return (
+//     <div className={css.widget}>
+//       <h1>Please Leave Feedback</h1>
+//       <div className={css.btnsWidget}>
+//         <button
+//           type="submit"
+//           className={`${css.btnWidget}  ${css.btnWidgetText}`}
+//         >
+//           Good
+//         </button>
+//         <button
+//           type="submit"
+//           className={`${css.btnWidget}  ${css.btnWidgetText}`}
+//         >
+//           Neutral
+//         </button>
+//         <button
+//           type="submit"
+//           className={`${css.btnWidget}  ${css.btnWidgetText}`}
+//         >
+//           Bad
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Widget;
